676ee661bc74ff01cf3b26171abe15aec445834a
xdrie <xdrie@alt.icu>
2020-10-20 23:59:30 -0700

    add comments to upload atlas

--- src/dusk/src/sprites.c
+++ src/dusk/src/sprites.c
@@ -13,6 +13,8 @@ void dusk_sprites_init() {
 }
 
 void dusk_sprites_upload_atlas(SpriteAtlas* atlas) {
+    // tile memory (can store 1024 tiles, or 32x32b, or 256x256p)
     memcpy(&tile_mem[4][0], atlas->tiles, atlas->tile_sz);
+    // object/sprite palette, in 8bpp stores 256 colors
     memcpy(&pal_obj_bank[0], atlas->pal, atlas->pal_sz);
 }
