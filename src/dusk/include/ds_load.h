c01892203185dfc831534192973aaec71b7bdb91
xdrie <xdrie@alt.icu>
2020-10-20 23:51:32 -0700

    add load_atlas

--- src/dusk/include/ds_load.h
+++ src/dusk/include/ds_load.h
@@ -8,4 +8,12 @@
 
 extern const GBFS_FILE *gbfs_dat;
 
+typedef struct SpriteAtlas {
+    u32* tiles;
+    u32 tile_sz;
+    u32* pal;
+    u32 pal_sz;
+} SpriteAtlas;
+
 void dusk_load_init(); // initialize loader
+SpriteAtlas dusk_load_atlas(char* filename);
--- src/dusk/src/load.c
+++ src/dusk/src/load.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <string.h>
 #include <tonc_types.h>
 #include "ds_load.h"
 
@@ -7,4 +8,19 @@ const GBFS_FILE *gbfs_dat;
 void dusk_load_init() {
     // locate the main GBFS archive
     gbfs_dat = find_first_gbfs_file(find_first_gbfs_file);
+}
+
+SpriteAtlas dusk_load_atlas(char* name) {
+    SpriteAtlas atlas;
+    char tiles_name[GBFS_ENTRY_SIZE + 10];
+    strcpy(tiles_name, name);
+    strcat(tiles_name, ".img.bin");
+    char pal_name[GBFS_ENTRY_SIZE + 10];
+    strcpy(pal_name, name);
+    strcat(pal_name, ".pal.bin");
+
+    atlas.tiles = gbfs_get_obj(gbfs_dat, tiles_name, &atlas.tile_sz);
+    atlas.pal = gbfs_get_obj(gbfs_dat, pal_name, &atlas.pal_sz);
+
+    return atlas;
 }
