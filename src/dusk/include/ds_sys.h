b2b1f836a1ac0bcafd1e56be32dc5eb990214deb
xdrie <xdrie@alt.icu>
2020-10-21 01:44:43 -0700

    add scene support

--- src/dusk/include/ds_sys.h
+++ src/dusk/include/ds_sys.h
@@ -6,5 +6,13 @@
 
 extern uint frame_count;
 
+typedef struct Scene {
+	void (*start)(void);
+	void (*end)(void);
+	void (*update)(void);
+} Scene;
+
 void dusk_init();  // initialize the system for using DUSK
 void dusk_frame(); // vsync
+void dusk_scene_set(Scene next);
+void dusk_scene_update();
--- src/dusk/src/sys.c
+++ src/dusk/src/sys.c
@@ -17,3 +17,31 @@ void dusk_frame() {
     vid_vsync();
     frame_count++;
 }
+
+static bool scene_changed = false;
+static void nothing(void) {}
+static Scene next_scene = {
+    .start = nothing,
+    .end = nothing,
+    .update = nothing,
+};
+static Scene current_scene = {
+    .start = nothing,
+    .end = nothing,
+    .update = nothing,
+};
+
+void dusk_scene_set(Scene next) {
+    scene_changed = true;
+    next_scene = next;
+}
+
+void dusk_scene_update() {
+    if (scene_changed) {
+        scene_changed = false;
+        current_scene.end();
+        current_scene = next_scene;
+        current_scene.start();
+    }
+    current_scene.update();
+}
