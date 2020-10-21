230268fd4f38597ca1f227070925a7d3510e1b5c
xdrie <xdrie@alt.icu>
2020-10-21 02:03:38 -0700

    support cycling demos

--- demos/src/main.c
+++ demos/src/main.c
@@ -1,12 +1,26 @@
 #include "dusk.h"
 #include "scenes.h"
 
+#define NUM_DEMOS 2
+
 int main() {
     dusk_init();
 
-    dusk_scene_set(fountain_scene);
-    
+    Scene demos[NUM_DEMOS] = {
+        fountain_scene,
+        fountain_scene
+    };
+    int demo_ix = 0;
+
+    dusk_scene_set(demos[demo_ix]);
+
     while (TRUE) {
+        key_poll();
+        if (key_was_down(KEY_START)) {
+            demo_ix = (++demo_ix) % NUM_DEMOS;
+            dusk_scene_set(demos[demo_ix]);
+        }
+
         dusk_scene_update();
     }
 }
