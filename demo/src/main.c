ef2df4c7f179fa2cd29fca9ba7012fb4fe3aaf8e
xdrie <xdrie@alt.icu>
2020-10-22 00:56:05 -0700

    remove linkflags

--- demo/src/main.c
+++ demo/src/main.c
@@ -1,15 +1,15 @@
 #include <stddef.h>
 
-size_t bleh(char* a) { return strlen(a); }
-
-size_t yeh(char* b) { return strlen(b); }
+char beef(char* g) { 
+    return g[0];
+}
 
 int main() {
     char* str_a = "cheese flakes";
     char* str_b = "applesauce";
     char* str_c = "cupcake";
 
-    size_t b = bleh(str_b); // 10
-    size_t a = yeh(str_a);  // 13
-    size_t c = yeh(str_c);  // 7
+    char a = beef(str_a); // 13
+    char b = beef(str_b); // 10
+    char c = beef(str_c); // 7
 }
