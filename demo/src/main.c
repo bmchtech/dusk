7b539637b572d7452db955cbe8159ace1a7eeac5
xdrie <xdrie@alt.icu>
2020-10-22 12:23:27 -0700

    use libgba printf

--- demo/src/main.c
+++ demo/src/main.c
@@ -1,4 +1,10 @@
-#include <stddef.h>
+#include <gba_console.h>
+#include <gba_video.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_input.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char beef(char* g) { 
     return g[0];
@@ -16,4 +22,20 @@ int main() {
     a = beef(str_a);
     b = beef(str_b);
     c = beef(str_c);
+
+    // the vblank interrupt must be enabled for VBlankIntrWait() to work
+	// since the default dispatcher handles the bios flags no vblank handler
+	// is required
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+
+	consoleDemoInit();
+
+	// ansi escape sequence to set print co-ordinates
+	// /x1b[line;columnH
+	iprintf("\x1b[10;10HTest:\n a=%d, b=%d, c=%d\n", a, b, c);
+
+	while (1) {
+		VBlankIntrWait();
+	}
 }
