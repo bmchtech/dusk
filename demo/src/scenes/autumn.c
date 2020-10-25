2cd103798be9e4e7c6e95897beac7ede1a67990f
xdrie <xdrie@alt.icu>
2020-10-24 21:16:10 -0700

    more leaves features

--- demo/src/scenes/autumn.c
+++ demo/src/scenes/autumn.c
@@ -2,8 +2,24 @@
 #include "dusk.h"
 
 SpriteAtlasLayout atlas_layout;
-const int NUM_LEAVES = 32;
 const int GRAVITY = 1;
+
+typedef struct LeafInfo {
+    s8 fall_points;
+    u8 max_fall_points;
+    u8 side_stage;
+    u8 side_prd;
+    s8 slant;
+    u8 slant_prd;
+    u16 col;
+} LeafInfo;
+
+#define NUM_LEAVES 32
+#define SIDE_STAGE_SZ 35
+
+const s8 SIDE_STAGES[SIDE_STAGE_SZ] = {5,  5,  5,  4,  4,  3,  3,  2, 1, 0, -1, -2, -3, -3, -4, -4, -5, -5,
+                                       -5, -4, -4, -3, -3, -2, -1, 0, 1, 2, 3,  3,  4,  4,  5,  5,  5};
+LeafInfo leaves[NUM_LEAVES];
 
 SpriteAtlasEntry* pick_random_leaf() {
     int entry_ix = qran_range(0, atlas_layout.num_entries);
@@ -20,6 +36,20 @@ Sprite* respawn_leaf(int i, int x, int y) {
             .y = y,
             .tid = dusk_sprites_pos_to_tid(leaf_entry->x, leaf_entry->y, atlas_layout.width, atlas_layout.height),
         });
+
+    leaves[i].max_fall_points = qran_range(0, 4);
+    leaves[i].side_prd = qran_range(2, 5);
+    leaves[i].col = x;
+    leaves[i].side_stage = qran_range(0, SIDE_STAGE_SZ);
+    sprites[i].x = leaves[i].col + SIDE_STAGES[leaves[i].side_stage];
+
+    // 30% to have slant
+    leaves[i].slant = 0;
+    if (qran_range(0, 10) <= 2) {
+        leaves[i].slant = qran_range(-1, 2);
+        leaves[i].slant_prd = qran_range(1, 3);
+    }
+
     return leaf;
 }
 
@@ -63,10 +93,21 @@ void autumn_update() {
             int iy = 0;
             respawn_leaf(i, ix, iy);
         }
-        // apply gravity
-        int grav_roll = qran_range(0, 3);
-        if (grav_roll == 0) {
+        // - motion
+
+        // 1. update fall points
+        leaves[i].fall_points--;
+        if (leaves[i].fall_points < 0) {
+            leaves[i].fall_points = qran_range(0, leaves[i].max_fall_points);
             sprites[i].y += GRAVITY;
+            if ((frame_count % leaves[i].slant_prd) == 0) {
+                leaves[i].col += leaves[i].slant;
+            }
+        }
+        // 2. update side stages
+        if ((frame_count % leaves[i].side_prd) == 0) {
+            leaves[i].side_stage = (leaves[i].side_stage + 1) % SIDE_STAGE_SZ;
+            sprites[i].x = leaves[i].col + SIDE_STAGES[leaves[i].side_stage];
         }
     }
 
