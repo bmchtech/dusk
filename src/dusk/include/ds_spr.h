77139238c7de4a6dc4c4a471e5c545a3f97d4c58
xdrie <xdrie@alt.icu>
2021-02-10 23:47:01 -0800

    update comments for ds_sprite

--- src/dusk/include/ds_spr.h
+++ src/dusk/include/ds_spr.h
@@ -13,28 +13,46 @@
 extern OBJ_ATTR obj_buffer[NUM_SPRITES];
 extern OBJ_AFFINE* obj_aff_buffer;
 
+/** sprite data */
 typedef struct Sprite {
     s16 x, y;
-    u16 tile_sz; // the number of tiles taken up by this sprite
+    /** the number of tiles taken up by this sprite */
+    u16 tile_sz;
     u16 tid;
-    u8 page; // used for spritesheets
+    /** used for spritesheets/animation */
+    u8 page;
 } Sprite;
 
+/** animation metadata */
 typedef struct Anim {
-    u8 start; // which page in the sprite the animation starts
-    u8 len;   // number of pages in this animation
-    u8 rate;  // (60/fps), or how many frames each page lasts
+    /** which page in the sprite the animation starts */
+    u8 start;
+    /** number of pages in this animation */
+    u8 len;
+    /** (60/fps), or how many frames each page lasts */
+    u8 rate;
 } Anim;
+
+/** define an animation, given a starting frame and frame count, and fps */
 #define MAKE_ANIM_EX(st, le, fps)                                                                                      \
     (Anim) { .start = st, .len = le, .rate = (60 / fps) }
-#define MAKE_ANIM(st, le) MAKE_ANIM_EX(st, le, 10) // default to 10fps
+/** define an animation, given a starting frame and frame count, at 10 fps */
+#define MAKE_ANIM(st, le) MAKE_ANIM_EX(st, le, 10)
 
+/** memory block used to track sprites */
 extern Sprite sprites[NUM_SPRITES];
 
-void dusk_sprites_init();                           // initialize sprite and OAM memory
-void dusk_sprites_upload_atlas(SpriteAtlas* atlas); // upload a sprite atlas to the sprite tiles and palettes
-Sprite* dusk_sprites_make(int index, u8 width, u8 height, Sprite spr); // create a sprite at index
-void dusk_sprites_sync(int index);                                     // synchronize sprite objects to OAM format
-void dusk_sprites_update();                                            // upload object data to OAM
-void dusk_sprites_anim_play(Sprite* spr, Anim* anim);                  // play frame animation on sprite
-u16 dusk_sprites_pos_to_tid(u16 x, u16 y, u16 sheet_width, u16 sheet_height); // get tid from spritesheet position
+/** initialize sprite and OAM memory */
+void dusk_sprites_init();
+/** upload a sprite atlas to tile and palette memory */
+void dusk_sprites_upload_atlas(SpriteAtlas* atlas);
+/** create a sprite and store it in index of sprite memory */
+Sprite* dusk_sprites_make(int index, u8 width, u8 height, Sprite spr);
+/** synchronize a sprite from the sprites array to OAM block (GPU memory) */
+void dusk_sprites_sync(int index);
+/** synchronize all sprites to OAM block, then upload OAM block to GPU */
+void dusk_sprites_update();
+/** play an animation on a sprite */
+void dusk_sprites_anim_play(Sprite* spr, Anim* anim);
+/** calculate the tid of a sprite given pos and sheet dimens */
+u16 dusk_sprites_pos_to_tid(u16 x, u16 y, u16 sheet_width, u16 sheet_height);
--- src/dusk/src/sprites.c
+++ src/dusk/src/sprites.c
@@ -19,16 +19,15 @@ void dusk_sprites_init() {
 }
 
 void dusk_sprites_upload_atlas(SpriteAtlas* atlas) {
+    // 1. upload the atlas tiles to tile memory
     // tile memory (can store 1024 tiles, or 32x32b, or 256x256p)
     memcpy(&tile_mem[4][0], atlas->tiles, atlas->tile_sz);
+    // 2. upload the atlas tile palette to palette memory
     // object/sprite palette, in 8bpp stores 256 colors
     memcpy(&pal_obj_bank[0], atlas->pal, atlas->pal_sz);
 }
 
 Sprite* dusk_sprites_make(int index, u8 width, u8 height, Sprite spr) {
-    // spr.w = width;
-    // spr.h = height;
-
     // automatically figure out size params
     spr.tile_sz = (width / 8) * (height / 8);
     u16 shape = 0;
