a9366ae10cd1a554126b4020bea730083c65b567
redthing1 <xhyinne@gmail.com>
2021-09-23 23:28:24 -0700

    even simpler repro

--- demo/DuskDemo/src/scenes/logo.c
+++ demo/DuskDemo/src/scenes/logo.c
@@ -39,8 +39,8 @@ void logo_start() {
     // ----------
 
 	REG_DISPCNT |= DCNT_BG1;
-	tte_init_chr4c(1, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_WHITE, NULL, NULL);
-	tte_init_con();
+	tte_init_chr4c(1, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_GRAY, NULL, NULL);
+    tte_init_con();
 
     tte_printf("test1");
 }
