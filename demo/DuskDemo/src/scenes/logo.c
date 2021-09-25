9ee67493c8537a273bc9a0ee31fbff387f82bacd
redthing1 <xhyinne@gmail.com>
2021-09-24 17:42:54 -0700

    explicitly specify font

--- demo/DuskDemo/src/scenes/logo.c
+++ demo/DuskDemo/src/scenes/logo.c
@@ -39,7 +39,7 @@ void logo_start() {
     // ----------
 
 	REG_DISPCNT |= DCNT_BG1;
-	tte_init_chr4c(1, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_WHITE, NULL, NULL);
+	tte_init_chr4c(1, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_WHITE, &verdana9Font, NULL);
 	tte_init_con();
 
     pal_gradient_ex(pal_bg_mem, 1, 4, CLR_YELLOW, CLR_ORANGE);
