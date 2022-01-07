9e3376aaa2658fb4839528fb3005ddce2534880c
redthing1 <xhyinne@gmail.com>
2022-01-06 21:13:57 -0800

    update log level enum

--- src/dusk/d_src/dusk/contrib/mgba.d
+++ src/dusk/d_src/dusk/contrib/mgba.d
@@ -8,13 +8,13 @@ enum REG_DEBUG_ENABLE = cast(vu16*) 0x4FFF780;
 enum REG_DEBUG_FLAGS = cast(vu16*) 0x4FFF700;
 enum REG_DEBUG_STRING = cast(char*) 0x4FFF600;
 
-enum MGBA_LOG_LEVEL
+enum MGBALogLevel
 {
-    MGBA_LOG_FATAL = 0,
-    MGBA_LOG_ERROR = 1,
-    MGBA_LOG_WARN = 2,
-    MGBA_LOG_INFO = 3,
-    MGBA_LOG_DEBUG = 4
+    FATAL = 0,
+    ERROR = 1,
+    WARN = 2,
+    INFO = 3,
+    DEBUG = 4
 }
 
 void mgba_printf (int level, const(char)* ptr, ...);
