a1706b654f8a17594d84d212f401d8c4667d4020
redthing1 <xhyinne@gmail.com>
2022-01-09 15:13:01 -0800

    assert always available

--- src/dusk/d_src/dusk/dbg.d
+++ src/dusk/d_src/dusk/dbg.d
@@ -2,23 +2,23 @@
 module dusk.dbg;
 
 extern (C) nothrow @nogc:
-version (DEBUG) {
-    private {
-        void mgba_printf(int level, const(char)* ptr, ...);
-        void exit(int status);
-    }
+private {
+    void mgba_printf(int level, const(char)* ptr, ...);
+    void exit(int status);
+}
 
-    pragma(inline, true) void ds_assert_func(bool cond, string msg, string file, int line) {
-        if (cond)
-            return;
+pragma(inline, true) void ds_assert_func(bool cond, string msg, string file, int line) {
+    if (cond)
+        return;
 
+    version (DEBUG) {
         // report fatal error to mgba
         mgba_printf(0, "Assertion failed at %s:%d: %s\n", cast(char*) file, line, cast(char*) msg);
-
-        exit(1);
     }
 
-    void ds_assert(bool cond, string msg = "", string file = __FILE__, int line = __LINE__) {
-        ds_assert_func(cond, msg, file, line);
-    }
+    exit(1);
+}
+
+void ds_assert(bool cond, string msg = "", string file = __FILE__, int line = __LINE__) {
+    ds_assert_func(cond, msg, file, line);
 }
