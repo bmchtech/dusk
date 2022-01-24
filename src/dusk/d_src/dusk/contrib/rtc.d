4b0e415ea6f635ad607130acb09af156e8d1431e
redthing1 <xhyinne@gmail.com>
2022-01-24 14:31:28 -0800

    fix rtc datetime read

--- src/dusk/d_src/dusk/contrib/rtc.d
+++ src/dusk/d_src/dusk/contrib/rtc.d
@@ -1,6 +1,7 @@
 module dusk.contrib.rtc;
 
 import tonc.tonc_types;
+import tonc.tonc_memmap;
 
 // #define RTC_EPOWER  ( 1 )
 // #define RTC_E12HOUR ( 2 )
@@ -29,33 +30,43 @@ extern (D) {
     // #define RTC_TM_HOUR( X )    ( ( ( X ) >> 16 ) & 0xff )
     // #define RTC_TM_MIN( X )     ( ( ( X ) >> 8 ) & 0xff )
     // #define RTC_TM_SEC( X )     ( ( ( X ) >> 0 ) & 0xff )
+    // #define BCD_DECODE( x ) ( ( ( x ) & 0xfu ) + ( ( ( x ) >> 4u ) * 10u ) )
+    // #define BCD_ENCODE( x ) ( ( ( x ) % 10 ) + ( ( ( x ) / 10 ) << 4 ) )
 
-    auto RTC_IN_YEAR(u64 x) {
+    auto RTC_TM_YEAR(u64 x) {
         return (x >> 48) & 0xff;
     }
 
-    auto RTC_IN_MON(u64 x) {
+    auto RTC_TM_MON(u64 x) {
         return (x >> 40) & 0xff;
     }
 
-    auto RTC_IN_MDAY(u64 x) {
+    auto RTC_TM_MDAY(u64 x) {
         return (x >> 32) & 0xff;
     }
 
-    auto RTC_IN_WDAY(u64 x) {
+    auto RTC_TM_WDAY(u64 x) {
         return (x >> 24) & 0xff;
     }
 
-    auto RTC_IN_HOUR(u64 x) {
+    auto RTC_TM_HOUR(u64 x) {
         return (x >> 16) & 0xff;
     }
 
-    auto RTC_IN_MIN(u64 x) {
+    auto RTC_TM_MIN(u64 x) {
         return (x >> 8) & 0xff;
     }
 
-    auto RTC_IN_SEC(u64 x) {
+    auto RTC_TM_SEC(u64 x) {
         return (x >> 0) & 0xff;
+    }
+
+    auto BCD_DECODE(u64 x) {
+        return (x & 0xfu) + ((x >> 4u) * 10u);
+    }
+
+    auto BCD_ENCODE(u64 x) {
+        return (x % 10) + ((x / 10) << 4);
     }
 }
 
@@ -87,15 +98,40 @@ struct RTCDateTime {
 
 extern (D) RTCDateTime rtc_get_datetime_ex() {
     RTCDateTime ret;
-    auto rawdt = rtc_get_datetime();
 
-    ret.year = cast(dt_field_size) RTC_IN_YEAR(rawdt);
-    ret.month = cast(dt_field_size) RTC_IN_MON(rawdt);
-    ret.day = cast(dt_field_size) RTC_IN_MDAY(rawdt);
-    ret.wday = cast(dt_field_size) RTC_IN_WDAY(rawdt);
-    ret.hour = cast(dt_field_size) RTC_IN_HOUR(rawdt);
-    ret.min = cast(dt_field_size) RTC_IN_MIN(rawdt);
-    ret.sec = cast(dt_field_size) RTC_IN_SEC(rawdt);
+    // disable interrupts
+    auto ime = *REG_IME;
+    *REG_IME = 0;
+    // get datetime from rtc hardware
+    auto rawdt = rtc_get_datetime();
+    // re-enable interrupts
+    *REG_IME = ime;
+
+    // ret.year = cast(dt_field_size) RTC_TM_YEAR(rawdt);
+    // ret.month = cast(dt_field_size) RTC_TM_MON(rawdt);
+    // ret.day = cast(dt_field_size) RTC_TM_MDAY(rawdt);
+    // ret.wday = cast(dt_field_size) RTC_TM_WDAY(rawdt);
+    // ret.hour = cast(dt_field_size) RTC_TM_HOUR(rawdt);
+    // ret.min = cast(dt_field_size) RTC_TM_MIN(rawdt);
+    // ret.sec = cast(dt_field_size) RTC_TM_SEC(rawdt);
+
+    // time.tm_year = BCD_DECODE( RTC_TM_YEAR( datetime ) ) + ( 2000u - 1900u );
+    // time.tm_mon = BCD_DECODE( RTC_TM_MON( datetime ) ) - 1;
+    // time.tm_mday = BCD_DECODE( RTC_TM_MDAY( datetime ) );
+    // time.tm_wday = BCD_DECODE( RTC_TM_WDAY( datetime ) );
+
+    // time.tm_hour = BCD_DECODE( RTC_TM_HOUR( datetime ) );
+    // time.tm_min = BCD_DECODE( RTC_TM_MIN( datetime ) );
+    // time.tm_sec = BCD_DECODE( RTC_TM_SEC( datetime ) );
+
+    ret.year = cast(dt_field_size) BCD_DECODE(RTC_TM_YEAR(rawdt));
+    ret.month = cast(dt_field_size) BCD_DECODE(RTC_TM_MON(rawdt));
+    ret.day = cast(dt_field_size) BCD_DECODE(RTC_TM_MDAY(rawdt));
+    ret.wday = cast(dt_field_size) BCD_DECODE(RTC_TM_WDAY(rawdt));
+
+    ret.hour = cast(dt_field_size) BCD_DECODE(RTC_TM_HOUR(rawdt));
+    ret.min = cast(dt_field_size) BCD_DECODE(RTC_TM_MIN(rawdt));
+    ret.sec = cast(dt_field_size) BCD_DECODE(RTC_TM_SEC(rawdt));
 
     return ret;
 }
